2c1727b7c037dc5f70027015c89df8d53a060e4f
umeshjoshi <umesh.joshi@digital.justice.gov.uk>
2018-04-12 13:57:17 +0100

    expended model for case 3,4 and 5

--- src/main/java/com/laa/model/crime/Weight.java
+++ src/main/java/com/laa/model/crime/Weight.java
@@ -2,24 +2,49 @@ package com.laa.model.crime;
 
 import java.math.BigDecimal;
 
-public class Weight {
+public enum AgeRange {
 
-    public static BigDecimal getWeight (int age) {
+    ZERO_TO_ONE(0, 1, BigDecimal.valueOf(0.15)),
+    TWO_TO_FOUR(2, 4, BigDecimal.valueOf(0.30)),
+    FIVE_TO_SEVEN(5, 7, BigDecimal.valueOf(0.34)),
+    EIGHT_TO_TEN(8, 10, BigDecimal.valueOf(0.38)),
+    ELEVEN_TO_TWELVE(11, 12, BigDecimal.valueOf(0.41)),
+    THIRTEEN_TO_FIFTEEN(13, 15, BigDecimal.valueOf(0.44)),
+    SIXTEEN_TO_EIGHTEEN(16, 18, BigDecimal.valueOf(0.59));
 
-        if (age >=0 && age <=1)
-            return BigDecimal.valueOf(0.15);
-        if (age >=2 && age <=4)
-            return BigDecimal.valueOf(0.30);
-        if (age >=5 && age <=7)
-            return BigDecimal.valueOf(0.34);
-        if (age >=8 && age <=10)
-            return BigDecimal.valueOf(0.38);
-        if (age >=11 && age <=12)
-            return BigDecimal.valueOf(0.41);
-        if (age >=13 && age <=15)
-            return BigDecimal.valueOf(0.44);
-        if (age >=16 && age <=18)
-            return BigDecimal.valueOf(0.59);
+    private int lowerBound;
+    private int upperBound;
+    private BigDecimal factor;
+
+    AgeRange(int lowerBound, int upperBound, BigDecimal factor) {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        this.factor = factor;
+    }
+
+    public BigDecimal weightValue(int lower, int upper) {
+
+        if (lower >= 0 && upper <= 1) {
+            return ZERO_TO_ONE.factor;
+        } else if (lower >= 2 && upper <= 4) {
+            return TWO_TO_FOUR.factor;
+        }
+        else if (lower >= 5 && upper <= 7) {
+            return FIVE_TO_SEVEN.factor;
+        }
+        else if (lower >= 8 && upper <= 10) {
+            return EIGHT_TO_TEN.factor;
+        }
+        else if (lower >= 11 && upper <= 12) {
+            return ELEVEN_TO_TWELVE.factor;
+        }
+        else if (lower >= 13 && upper <= 15) {
+            return THIRTEEN_TO_FIFTEEN.factor;
+        }
+        else if (lower >= 16 && upper <= 18) {
+            return SIXTEEN_TO_EIGHTEEN.factor;
+        }
         return null;
     }
+
 }
